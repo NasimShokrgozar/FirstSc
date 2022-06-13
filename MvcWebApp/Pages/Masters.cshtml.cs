7026b64a24961c246097eea26c5d5416c567d73a
Shokrgozar.Nasim <Shokrgozar.Nasim@PKS001P2041>
2022-06-13 16:52:03 +0430

    Add project files.

---
+++ MvcWebApp/Pages/Masters.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace MvcWebApp.Pages
+{
+    public class MastersModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
