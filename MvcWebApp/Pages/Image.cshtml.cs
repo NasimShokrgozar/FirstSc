8f2337bcdbee33191d3ee6cefa9ec345dc816169
Nasim Shokrgozar <nasim.shokrgozar@outlook.com>
2022-06-14 10:17:20 +0430

    Image added

---
+++ MvcWebApp/Pages/Image.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace MvcWebApp.Pages
+{
+    public class ImageModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
